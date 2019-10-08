4d73b2c395b5103d6d9b4f91c89571ccfc9637b0
Moin-A <Quintype@101992>
2019-10-08 17:22:06 +0530

    changes

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -18,26 +18,23 @@ export class Experience extends Component {
                         <h4 className="card__heading">
                          <span className="card__heading--span">Web Technology</span> 
                         </h4>
-                        <div className="card__details card__picture--3">Details</div>
-                     Front
+                        <div className="card__details">
+                       
+                        </div>
+                
                         </div>
                         <div className="cardside cardside--back">
-                     Back
-                        </div>   
+                        <div className="card__details">
+                        <ul>
+                            <li>HTML5</li>
+                            <li>css3</li>
+                            <li>sass</li>
+                            <li>Flexbox</li>
+                            <li>Grid</li>
+                            <li>Media Queries</li>
+                        </ul>
                         </div>
-                        </div>
-                        <div className="column">
-                        <div className="card card--1">
-                        <div className="cardside cardside--front">
-                        <div className="card__picture card__picture--1">&nbsp;</div>
-                        <h4 className="card__heading">
-                         <span className="card__heading--span">Web Technology</span> 
-                        </h4>
-                            <div className="card__details card__picture--3">Details</div>
-                     Front
-                        </div>
-                        <div className="cardside cardside--back">
-                     Back
+             
                         </div>   
                         </div>
                         </div>
@@ -46,6 +43,36 @@ export class Experience extends Component {
                         <div className="cardside cardside--front">
                         <div className="card__picture card__picture--1">&nbsp;</div>
                         <h4 className="card__heading">
+                         <span className="card__heading--span">Javascript</span> 
+                        </h4>
+                            <div className="card__details">Details</div>
+                     Front
+                        </div>
+                        <div className="cardside cardside--back">
+                         <div className="card__details">
+                        <ul>
+                            <li>Arrow Function</li>
+                            <li>Template Strings</li>
+                            <li>Destructuring</li>
+                            <li>Flexbox</li>
+                            <li>Iterables & Looping</li>
+                            <li>SPread</li>
+                            <li>Promises</li>
+                            <li>Javascript Modules</li>
+                            <li>Classes</li>
+                            <li>Maps & Sets</li>
+                            <li>Async  Await </li>
+                        </ul>
+                        </div>
+                
+                        </div>   
+                        </div>
+                        </div>
+                        <div className="column">
+                        <div className="card card--3">
+                        <div className="cardside cardside--front">
+                        <div className="card__picture card__picture--1">&nbsp;</div>
+                        <h4 className="card__heading">
                          <span className="card__heading--span">Web Technology</span> 
                         </h4>
                             <div className="card__details card__picture--3">Details</div>
@@ -57,7 +84,7 @@ export class Experience extends Component {
                         </div>
                         </div>
                         <div className="column">
-                        <div className="card card--3">
+                        <div className="card card--4">
                         <div className="cardside cardside--front">
                             <div className="card__picture card__picture--1">&nbsp;</div>
                             <h4 className="card__heading">
